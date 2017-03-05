b67e155fd605db694581952ec96296408dfbda47
Kevin Chavez <kjchavez@stanford.edu>
2017-03-05 08:52:35 -0800

    added comments to run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,4 @@
+# Note: if SFML libs are already in your typical library path, then you can
+# simply run the executable. Otherwise, leaving this out will result in an error
+# when trying to load shared libraries.
 export LD_LIBRARY_PATH=/home/kevin/Code/SFML/lib:$LD_LIBRARY_PATH && ./bin/sfml
